d2231a1137c5e0ac4a1a45a7ee4acccd57a49cc3
jhonataniberico <jhonatanibericom@gmail.com>
2018-02-16 16:59:07 -0500

    cambios en el js

--- public/js/index_es.js
+++ public/js/index_es.js
@@ -192,18 +192,18 @@ function isEmpty(val){
 }
 
 function limpiarCampos(){
-	var nombre_completo = $('#nombre_completo').val("");
-	var empresa  		= $('#empresa').val("");
-	var email 	 		= $('#email').val("");
-	var pais 	 		= $('#pais').val("");
-	var cargo 	 		= $('#cargo').val("");
-	var telefono 		= $('#telefono').val("");
-	var relacion		= $('#relacion').val("0");
+	$('#nombre_completo').val("");
+	$('#empresa').val("");
+	$('#email').val("");
+	$('#pais').val("");
+	$('#cargo').val("");
+	$('#telefono').val("");
+	$('#relacion').val("0");
 	$('.selectpicker').selectpicker('refresh');
 	var c_email    		= $('#c-email').is(':checked');
 	var c_telefono    	= $('#c-telefono').is(':checked');
 	var c_ambos    		= $('#c-ambos').is(':checked');
-	var terminos		= $('#checkbox-1').is(':checked');
+	$('#checkbox-1').is(':checked');
 	if(c_email == true){
 		$('#c-email').parent().removeClass('is-checked');
 	}else if(c_telefono == true){
