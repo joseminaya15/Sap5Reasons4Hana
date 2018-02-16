b769a1fbbb56cc75aa97c9967ef9fce80a011b5b
jhonataniberico <jhonatanibericom@gmail.com>
2018-02-16 16:20:48 -0500

    cambios en el js

--- public/js/index_pt.js
+++ public/js/index_pt.js
@@ -114,11 +114,9 @@ function soloLetras(e) {
  }
  function valida(e) {
     tecla = (document.all) ? e.keyCode : e.which;
-    //Tecla de retroceso para borrar, siempre la permite
     if (tecla==8){
         return true;
     }
-    // Patron de entrada, en este caso solo acepta números
     patron =/[0-9]/;
     tecla_final = String.fromCharCode(tecla);
     return patron.test(tecla_final);
