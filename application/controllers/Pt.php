deab19730b49ddb86d6772e2d266ed7a6890f659
jhonataniberico <jhonatanibericom@gmail.com>
2018-02-20 12:45:44 -0500

    emails comentados de las 3 vistas

--- application/controllers/Pt.php
+++ application/controllers/Pt.php
@@ -79,32 +79,32 @@ class Pt extends CI_Controller {
         echo json_encode($data);
     }
 
-    function sendEmail(){
-        $data['error'] = EXIT_ERROR;
-        $data['msj']   = null;
-        try {
-            $this->load->library('email');
-            $configGmail = array('protocol'  => 'smtp',
-                                 'smtp_host' => 'smtpout.secureserver.net',
-                                 'smtp_port' => 3535,
-                                 'smtp_user' => 'confirmaciones@merino.com.pe',
-                                 'smtp_pass' => 'cFm$17Pe',
-                                 'mailtype'  => 'html',
-                                 'charset'   => 'utf-8',
-                                 'newline'   => "\r\n");
-            $this->email->initialice($configGmail);
-            $this->email->from('info@sap-latam.com');
-            $this->email->to('jhonatanibericom@gmail.com');
-            $this->email->subject('Estoy interesado en SAP 5 Reasons 4 Hana');
-            $texto = 'html';
-            $this->email->message($texto);
-            $this->email->send();
-            $data['error'] = EXIT_SUCCESS;
-        }catch(Exception $e) {
-            $data['msj'] = $e->getMessage();
-        }
-        return json_encode(array_map('utf8_encode', $data));
-    }
+    // function sendEmail(){
+    //     $data['error'] = EXIT_ERROR;
+    //     $data['msj']   = null;
+    //     try {
+    //         $this->load->library('email');
+    //         $configGmail = array('protocol'  => 'smtp',
+    //                              'smtp_host' => 'smtpout.secureserver.net',
+    //                              'smtp_port' => 3535,
+    //                              'smtp_user' => 'confirmaciones@merino.com.pe',
+    //                              'smtp_pass' => 'cFm$17Pe',
+    //                              'mailtype'  => 'html',
+    //                              'charset'   => 'utf-8',
+    //                              'newline'   => "\r\n");
+    //         $this->email->initialice($configGmail);
+    //         $this->email->from('info@sap-latam.com');
+    //         $this->email->to('jhonatanibericom@gmail.com');
+    //         $this->email->subject('Estoy interesado en SAP 5 Reasons 4 Hana');
+    //         $texto = 'html';
+    //         $this->email->message($texto);
+    //         $this->email->send();
+    //         $data['error'] = EXIT_SUCCESS;
+    //     }catch(Exception $e) {
+    //         $data['msj'] = $e->getMessage();
+    //     }
+    //     return json_encode(array_map('utf8_encode', $data));
+    // }
 
     function cambiarIdioma(){
       $data['error'] = EXIT_ERROR;
