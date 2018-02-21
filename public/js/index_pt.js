843a8d3e13a3f6c5fc528fa7bcb65f49a46324b7
jhonataniberico <jhonatanibericom@gmail.com>
2018-02-21 13:08:27 -0500

    cambios en el js

--- public/js/index_pt.js
+++ public/js/index_pt.js
@@ -116,17 +116,17 @@ function solicitarEstimacion(){
 	}
 	$('.button-confirmar').prop("disabled", true);
 	$.ajax({
-		data  : { nombre_completo : nombre_completo,
-				  empresa 	      : empresa,
-				  email 		  : email,
-				  pais 			  : pais,
-				  cargo 		  : cargo,
-				  telefono 		  : telefono,
-				  relacion 		  : relacion,
-				  contacto 		  : contacto,
-				  term_cond       : term_cond },
-		url   : 'pt/solicitarEstimacion',
-		type  : 'POST'
+		data : {nombre_completo : nombre_completo,
+				empresa 	    : empresa,
+				email 		  	: email,
+				pais 			: pais,
+				cargo 		  	: cargo,
+				telefono 		: telefono,
+				relacion 		: relacion,
+				contacto 		: contacto,
+				term_cond       : term_cond},
+		url  : 'pt/solicitarEstimacion',
+		type : 'POST'
 	}).done(function(data){
 		try{
         	data = JSON.parse(data);
@@ -224,9 +224,9 @@ function cambiarIdioma(){
 		location.href = 'Pt';
 	}
 	$.ajax({
-		data  : {idioma : idioma},
-		url   : 'pt/cambiarIdioma',
-		type  : 'POST'
+		data : {idioma : idioma},
+		url  : 'pt/cambiarIdioma',
+		type : 'POST'
 	}).done(function(data){
 		try{
         data = JSON.parse(data);
