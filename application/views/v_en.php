29fc493b7720ff23575576ec0fac0ba2c9eaa058
jhonataniberico <jhonatanibericom@gmail.com>
2018-02-21 10:35:37 -0500

    cambios en la vista google analytics

--- application/views/v_en.php
+++ application/views/v_en.php
@@ -22,6 +22,14 @@
 	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>animate.css?v=<?php echo time();?>">
 	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>m-p.min.css?v=<?php echo time();?>">
 	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>index.css?v=<?php echo time();?>">
+	<script type="text/javascript">
+		/*(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-114404560-1', 'auto');
+			ga('send', 'pageview');*/
+	</script>
 </head>
 <body>
 	<section id="principal">
