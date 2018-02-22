1f2871e29c430ae5e964c401a0528ccf4202cb93
jhonataniberico <jhonatanibericom@gmail.com>
2018-02-22 17:17:00 -0500

    cambios en js en y pt

--- public/js/index_pt.js
+++ public/js/index_pt.js
@@ -171,7 +171,7 @@ function soloLetras(e){
     tecla_final = String.fromCharCode(tecla);
     return patron.test(tecla_final);
 }
-function validateEmail(email) {
+function validateEmail(email){
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
