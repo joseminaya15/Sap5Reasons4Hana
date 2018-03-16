4570530ef02790c3a2622b54878f7e2fb0f8e9a3
joseminaya15 <jose.minayac15@gmail.com>
2018-03-16 12:12:58 -0500

    css

--- application/views/v_es.php
+++ application/views/v_es.php
@@ -35,16 +35,15 @@
 	<section id="principal">
 		<div class="header">
 			<div class="header-left">
-				<img class="logo-header" src="<?php echo RUTA_IMG?>logo/logo_favicon.png">
-				<h2>SAP S/4 HANA</h2>
+				<img class="logo-header" src="<?php echo RUTA_IMG?>logo/logo_home.png">
 			</div>
 			<div class="header-right">
 				<button class="mdl-button mdl-js-button mdl-button--icon button-home" onclick="returnHome()"><i class="mdi mdi-home"></i></button>
 				<div class="mdl-idioma">
 					<select class="selectpicker" id="Idioma"  name="Idioma" onchange="cambiarIdioma()">
 						<option value="Español">Espa&ntilde;ol</option>
-						<option value="Inglés" disabled="true">Ingl&eacute;s</option>
-						<option value="Portugués" disabled="true">Portugu&eacute;s</option>
+						<option value="Inglés" disabled="true">English</option>
+						<option value="Portugués" disabled="true">Portugu&ecirc;s</option>
 					</select>
 				</div>
 				<div class="background3"></div>
