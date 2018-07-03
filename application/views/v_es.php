73d7d0f99cc75f59ba057695e825a4cf3146ef7e
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-07-03 18:58:12 -0500

    cambios

--- application/views/v_es.php
+++ application/views/v_es.php
@@ -24,8 +24,7 @@
 	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>index.css?v=<?php echo time();?>">
 	<!--Google Tag Manager -->
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PW9PLW6');</script><!--End Google Tag Manager -->
-	<!--Google Tag Manager  (noscript) -->
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PW9PLW6"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><!--End Google Tag Manager  (noscript) -->
+	<!--Google Tag Manager  (noscript) --><!--End Google Tag Manager  (noscript) -->
 </head>
 <body>
 	<section id="principal">
@@ -42,6 +41,7 @@
 						<option value="Portugués">Portugu&ecirc;s</option>
 					</select>
 				</div>
+				<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PW9PLW6"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 				<div class="background1"></div>
 				<div class="background2"></div>
 				<div class="background3"></div>
@@ -959,6 +959,10 @@
         	$('select').selectpicker('mobile');
         } else {
             $('select').selectpicker();
+        }
+        var URLactual = window.location;
+        if(URLactual['href'] != 'http://www.sap-latam.com/5reasons_hana/es'){
+            location.href = 'http://www.sap-latam.com/5reasons_hana/es';
         }
         $(window).load(function(){
         	if(<?php echo $pantalla ?> == 5) {
